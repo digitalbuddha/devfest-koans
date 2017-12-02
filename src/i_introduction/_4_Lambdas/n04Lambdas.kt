d64757e0e5c0ddf1ce0e5001d6a1adf8c1cb5735
Brian Plummer <brian.plummer@nike.com>
2017-12-02 11:45:43 -0500

    remove lambda answer

--- src/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ src/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -21,4 +21,4 @@ fun todoTask4(collection: Collection<Int>): Nothing = TODO(
     documentation = doc4(),
     references = { Finder().containsEven(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = TODO()//collection.any { it % 2 == 0; }
+fun task4(collection: Collection<Int>): Boolean = TODO()
